1d52506573636cdc46f27263e7de84e1ba1eb29f
Kayla Luebbert <luebbertkayla@gmail.com>
2021-03-15 20:32:38 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -17,14 +17,16 @@ class Transfer
   end
   
   def execute_transaction
-  
+    if 
+    @sender.deposit( @amount * -1 ) 
+    @receiver.deposit( @amount )
+    @status = "complete"
     elsif @status == "complete"
       puts "Transaction was already excuted"
     else
-        if @sender.balance < @amount
+      @sender.balance < @amount
       @status == "rejected"
       return "Transaction rejected. Please check your account balance."
-     
     end
   end
 end
