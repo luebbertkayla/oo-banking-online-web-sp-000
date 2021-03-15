2612f77382b2ae72cf3b53c9e8b9d41afbaabfed
Kayla Luebbert <luebbertkayla@gmail.com>
2021-03-15 20:42:22 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -25,7 +25,7 @@ class Transfer
       @status = "rejected"
       puts "Transaction rejected. Please check your account balance."
    
-    else
+    elsif
       @sender.deposit( @amount * -1 ) 
       @receiver.deposit( @amount )
       @status = "complete"
